d12deeff003c0d33c8030e50a9a3c26932b086eb
Shannon Nabors <shannon.nabors@gmail.com>
2020-04-21 18:34:41 -0400

    adding lecture code

--- newsletter.rb
+++ newsletter.rb
@@ -1,3 +1,4 @@
+require 'pry'
 #########################
 # Data for the newsletter
 #########################
@@ -6,6 +7,7 @@ CAMPUS = {
   "name": "DC",
   "address": "1440 G St NW, Washington, DC 20005",
 }
+
 DATE = "Nov 20, 2019"
 
 SUBSCRIBERS = ["rhona@grimes.info", "cedricschmidt@robel.io", "edmond@ko.org", "bryant@cummingsfisher.biz", "alverta@bernhard.name"]
@@ -26,15 +28,21 @@ ARTICLES = [
 #########################
 
 def calculate_recipients
+  # SUBSCRIBERS.select do |email|
+  #   present_in_unsubscribed = UNSUBSCRIBED.include?(email)
+  #   !present_in_unsubscribed
+  # end
+  SUBSCRIBERS - UNSUBSCRIBED
   # Using the SUBSCRIBERS and UNSUBSCRIBED arrays,
   # write a method that will return an array of only the subscribers who haven't unsubscribed
 end
 
-def first_n_articles(number_of_articles
-  ARTICLES.first(number_of_articles)
+def first_n_articles(number_of_articles)
+  ARTICLES.first(number_of_articles.to_i)
 end
 
 def print_recipients
+  puts calculate_recipients.join(", ")
   # Write a method that uses the output of calculate_recipients
   # and returns a list of emails separated by commas
   # Ex) "abc@email.com, def@email.com, ghi@email.com"
@@ -44,15 +52,22 @@ def print_one_article(article)
   # Write a method that will take an article hash
   # and print the title, author and text as a formatted string
   # See the README/sample output for examples
+  puts article[:title]
+  puts "by: #{article[:author]}"
+  puts article[:text]
+  puts
 end
 
 def print_many_articles(articles)
   # Write a method that will take in an array of article hashes
   # and format each one using the print_one_article method
+  articles.each do |article|
+    print_one_article(article)
+  end
 end
 
 def format_campus_location(campus)
-  "Flatiron #{campus["name"]}"
+  "Flatiron #{campus[:name]}"
 end
 
 def format_subject
@@ -77,8 +92,6 @@ def print_newsletter(number)
   articles = first_n_articles(number)
   print_many_articles(articles)
   puts format_footer(CAMPUS)
-
-  end
 end
 
 def run
